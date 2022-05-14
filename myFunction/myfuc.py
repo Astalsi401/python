1f67dc952afba2d05d6c6522adda748e9d6df9a3
Astalsi401 <misting401@gmail.com>
2022-05-14 14:06:26 +0800

    update

--- myFunction/myfuc.py
+++ myFunction/myfuc.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from csv import writer, reader
+from json import dump
 from datetime import date
 from openpyxl import load_workbook
 from time import sleep
@@ -25,6 +26,26 @@ def readCsv(path, name):
     return a
 
 
+def writeJson(path, json, name, mode='w+', encoding='utf-8-sig'):
+    if not os.path.isdir(f'{path}/json'):
+        os.makedirs(f'{path}/json')
+    with open(f'{path}/{name}', mode, encoding=encoding) as f:
+        dump(json, f, ensure_ascii=False)
+        print(f'{path}/{name} saved!')
+
+
+def convertToJson(data):
+    json = []
+    for a in data:
+        i = 0
+        b = {}
+        for c in a:
+            b[f'row{i}'] = c
+            i += 1
+        json.append(b)
+    return json
+
+
 def getXlsxSheets(path, name):
     wb = load_workbook(f'{path}/{name}.xlsx')
     sheetnames = wb.get_sheet_names()
@@ -32,6 +53,13 @@ def getXlsxSheets(path, name):
         for b in sheetnames:
             f.write(f'{b}\n')
     print(f'{name}_sheetname.txt saved')
+
+
+def getFilesName(path, ext=None):
+    if ext == None:
+        return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    else:
+        return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f'.{ext}' in f]
 
 
 def yearsCalc(yearsAgo=0):
---
+++ projects/convertFiles/csvToJson/csvToJson.py
@@ -0,0 +1,56 @@
+from datetime import date
+import os
+from csv import writer, reader
+from json import dump, load
+
+pwd = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
+
+
+def writeCsv(path, name, data, mode='w+'):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    with open(f'{path}/{name}', mode=f'{mode}', encoding='utf-8-sig', newline='') as f:
+        for a in data:
+            writer(f).writerow(a)
+    print(f'{path}/{name} saved!')
+
+
+def readCsv(path, name):
+    a = []
+    with open(f'{path}/{name}', mode='r', encoding='utf-8-sig', newline='') as f:
+        b = reader(f)
+        for c in b:
+            a.append(c)
+    return a
+
+
+def getFilesName(path, ext=None):
+    if ext == None:
+        return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    else:
+        return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f'.{ext}' in f]
+
+
+def convertToJson(data):
+    json = []
+    for a in data:
+        i = 0
+        b = {}
+        for c in a:
+            b[f'row{i}'] = c
+            i += 1
+        json.append(b)
+    return json
+
+
+def writeJson(path, json, name, mode='w+', encoding='utf-8-sig'):
+    if not os.path.isdir(f'{path}'):
+        os.makedirs(f'{path}')
+    name = name.replace('.csv', '.json')
+    with open(f'{path}/{name}', mode, encoding=encoding) as f:
+        dump(json, f, ensure_ascii=False)
+        print(f'{path}/{name} saved!')
+
+
+for name in getFilesName(f'{pwd}/csv', 'csv'):
+    writeJson(f'{pwd}/json', convertToJson(readCsv(f'{pwd}/csv', f'{name}')), f'{name}')
